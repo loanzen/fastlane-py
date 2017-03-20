3346ca677d3434ac2f5103b1cf4e19296af4df0c
Kshitij Mittal <kmittal@Kshitijs-MacBook-Pro.local>
2017-03-20 15:14:58 +0530

    __init__.py added, gitignore added

--- fastlane.py
+++ fastlane.py
@@ -1,22 +1,26 @@
-Class FastlaneClient(object):
+import base64
+import requests
+
+class FastlaneClient(object):
 	def __init__(self,env,username,password):
 		self.env = env
 		self.username = username
 		self.password = password
-	def getVehicleDetails():
-		import requests
+	def getVehicleDetails(self,regn_no):
 
-		url = "https://web.fastlaneindia.com/sandbox/api/v1.2/vehicle"
+		if self.env == "production":
+			url = "https://web.fastlaneindia.com/vin/api/v1.2/vehicle"
+		elif self.env == "sandbox":
+			url = "https://web.fastlaneindia.com/sandbox/api/v1.2/vehicle"
+		else:
+			return "invalid_env"
 
-		querystring = {"regn_no":"DL10CB3330"}
-
+		querystring = {"regn_no":regn_no}
 		headers = {
 		    'accept': "application/json",
-		    'authorization': "Basic TjAxMVRFU1QxOm4xMWxuekA3MyQ=",
-		    'cache-control': "no-cache",
-		    'postman-token': "bc450ac9-6773-e49b-e831-ad62878a75f9"
-		    }
+		    'authorization': "Basic " + base64.b64encode(self.username + ':' + self.password),
+		}
 
 		response = requests.request("GET", url, headers=headers, params=querystring)
 
-		print(response.text)
+		return response.text.status
