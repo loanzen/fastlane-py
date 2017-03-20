961fb4308015f1baaed9d078faeb8724ad9d5765
Kshitij Mittal <kmittal@Kshitijs-MacBook-Pro.local>
2017-03-20 19:41:55 +0530

    docs added

--- fastlane/fastlane.py
+++ fastlane/fastlane.py
@@ -5,12 +5,21 @@ import requests
 
 class FastlaneClient(object):
     def __init__(self, env, username, password):
+        """
+        Takes env, username and password as the parameters, where:
+        evn = sandbox|production
+        username & password: credentials of the API [allotted by
+                            fastlaneindia.com]
+        """
         self.env = env
         self.username = username
         self.password = password
 
     def get_vehicle_details(self, regn_no):
-
+        """
+        Takes reg_no as the parameter to get the details of the vehicle from
+        fastlane apis.
+        """
         if self.env == "production":
             url = "https://web.fastlaneindia.com/vin/api/v1.2/vehicle"
         elif self.env == "sandbox":
@@ -28,4 +37,4 @@ class FastlaneClient(object):
         response = requests.request("GET", url, headers=headers,
                                     params=querystring)
 
-        return response.text
+        return response
